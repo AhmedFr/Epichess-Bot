504639e8f95604bcb1915275c871e4421c76dd6e
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2022-09-02 23:04:21 +0200

    FIX: Help command reply

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,7 +5,10 @@ module.exports = {
     .setName("help")
     .setDescription("Show all the bots commands and their description."),
   async execute(interaction) {
-    const user = interaction.options.getUser('oponent'); 
-    await interaction.reply({ content: '', ephemeral: true });
+    const user = interaction.options.getUser("oponent");
+    await interaction.reply({
+      content: `/challenge @PSEUDO (lien du jeu): pour defier un ami à une partie classé.\n/friendly @PSEUDO (lien du jeu): pour defier un ami à une partie sans enjeu.\n/info @PSEUDO: pour afficher l'elo et pseudo chess.com & lichess d'un membre du serveur.\n/add PLATEFORME PSEUDO: pour ajouter le pseudo que vous utiliser pour X plateforme (PLATEFORME  étant chesscom  ou lichess).`,
+      ephemeral: true,
+    });
   },
 };
